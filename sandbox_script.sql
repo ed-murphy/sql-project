3669e37bd2f89f8058a6b126e13302686c089ee3
ed-murphy <edward.v.murphy@gmail.com>
2025-08-02 14:00:09 -0400

    first commit

---
+++ sandbox_script.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM (
+    SELECT *
+    FROM january_jobs
+    UNION ALL
+    SELECT *
+    FROM february_jobs
+    UNION ALL
+    SELECT *
+    FROM march_jobs
+) AS quarter1_job_postings
